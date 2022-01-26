4868563748da796b761c250fa14f6c3a6b046556
nepp.plus <nepp.plus@gmail.com>
2022-01-26 16:20:31 +0900

    카테고리 수정 작업중

--- js/product/categoryManage.js
+++ js/product/categoryManage.js
@@ -15,7 +15,7 @@ let getAllLargetCategoryList = () => {
         <span>${element.id}</span>
         <span><img src='${element.icon_url}' style='width:40px;height:40px;'></span>
         <span>${element.name}</span>
-        <button categoryId=${element.id}>수정</button>
+        <button categoryId=${element.id} onclick={editLargeCategory(${element.id})}>수정</button>
         </li>
         `;
 
@@ -27,7 +27,7 @@ let getAllLargetCategoryList = () => {
     });
 };
 
-let postLargetCategory = () => {
+let postLargeCategory = () => {
   const form = new FormData($("#categoryForm")[0]);
   myAxios
     .post("/admin/largecategory", form)
@@ -35,6 +35,30 @@ let postLargetCategory = () => {
       alert("대분류 추가 완료");
 
       let url = `/html/product/category_add.html`;
+      $(location).attr("href", url);
+    })
+    .catch(function (error) {
+      alert(error.response.data.message);
+    });
+};
+
+let patchLargeCategory = (field, largeCategoryId) => {
+  let form = null;
+
+  if (field == "name") {
+    form = new FormData($("#editCategoryNameForm")[0]);
+  } else if (field == "icon_url") {
+    form = new FormData($("#editCategoryIconForm")[0]);
+  }
+
+  form.append("large_category_id", largeCategoryId);
+  form.append("field", field);
+  myAxios
+    .patch("/admin/largecategory", form)
+    .then(function (res) {
+      alert("대분류 수정 완료");
+
+      let url = `/html/product/large_category_manage.html`;
       $(location).attr("href", url);
     })
     .catch(function (error) {
