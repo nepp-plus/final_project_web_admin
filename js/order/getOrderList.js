f43ccd55468dcd68dd5460ae152634c77e890e35
nepp.plus <nepp.plus@gmail.com>
2022-02-03 12:39:29 +0900

    /admin/order로 우선 변경

--- js/order/getOrderList.js
+++ js/order/getOrderList.js
@@ -4,43 +4,18 @@ $(document).ready(() => {
   $("#endDate").val(now.toISOString().substr(0, 10));
 });
 
-let getOrderList = (status) => {
+let getOrderList = async (status) => {
   let sdate = $("#startDate").val();
   let edate = $("#endDate").val();
 
   console.log(sdate);
   console.log(edate);
 
-  myAxios
-    .get("/admin/shipment", {
-      params: {
-        status: status,
-      },
-    })
-    .then(function (res) {
-      let shipmentArr = res.data.data.shipment;
+  let res = await myAxios.get("/admin/order", {
+    params: {
+      status: status,
+    },
+  });
 
-      let orderListUl = $("#orderList");
-      orderListUl.empty();
-      shipmentArr.forEach((element) => {
-        console.log(element);
-
-        let li = `
-        <tr>
-        <td>${element.id}</td>
-        <td>${element.order.receiver_name}</td>
-        <td>${element.order.receiver_name}</td>
-        <td>${element.order.address}</td>
-        <td>(우편번호와야댐)</td>
-        <td>${element.order.phone_num}</td>
-        <div>
-        </tr>
-        `;
-
-        orderListUl.append(li);
-      });
-    })
-    .catch(function (error) {
-      alert(error.response.data.message);
-    });
+  return res.data.data.order;
 };
